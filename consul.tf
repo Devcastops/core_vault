fe4b24ad2791b3c44a4d3c583950709c00f22284
James Sandford <sandford.james@gmail.com>
2024-05-13 19:28:38 +0100

    fix: setting scheme

--- consul.tf
+++ consul.tf
@@ -2,8 +2,9 @@ resource "vault_consul_secret_backend" "core_consul" {
   path        = "core/consul"
   description = "Manages the Consul backend"
 
-  address = var.consul_url
+  address = split("://",var.consul_url)[1]
   token   = var.consul_token
+  scheme  = "${split("://",var.consul_url)[0]}://"
 }
 
 resource "vault_consul_secret_backend_role" "admin" {
