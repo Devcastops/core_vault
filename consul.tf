a43f83a38e8524226f89c09a874ca84ce9c4fcde
James Sandford <sandford.james@gmail.com>
2024-05-13 19:30:16 +0100

    fix: doesnt need ://

--- consul.tf
+++ consul.tf
@@ -4,7 +4,7 @@ resource "vault_consul_secret_backend" "core_consul" {
 
   address = split("://",var.consul_url)[1]
   token   = var.consul_token
-  scheme  = "${split("://",var.consul_url)[0]}://"
+  scheme  = split("://",var.consul_url)[0]
 }
 
 resource "vault_consul_secret_backend_role" "admin" {
