a68e0d425dd4d94c5045143c32bfccae68693ff9
James Sandford <sandford.james@gmail.com>
2024-08-19 20:32:02 +0100

    fix: try client id

--- consul.tf
+++ consul.tf
@@ -57,6 +57,6 @@ resource "vault_identity_oidc_provider" "consul_auto_config" {
 resource "vault_identity_oidc_role" "consul_auto_config" {
   name = "consul-cluster-dc1"
   key = vault_identity_oidc_key.consul_auto_config.name
-  client_id = vault_identity_oidc_client.consul_auto_config.id
+  client_id = vault_identity_oidc_client.consul_auto_config.client_id
   template = "{\"consul\": {\"hostname\": \"consul-client\" } }"
 }
