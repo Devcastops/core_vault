c95d4e54db26ecf8aee4677fc38235424c366f0c
James Sandford <sandford.james@gmail.com>
2024-10-20 10:40:23 +0100

    feat: start add vault oidc support

---
+++ github.tf
@@ -0,0 +1,29 @@
+locals {
+  github_access = {
+    packer_images = {
+      policies = ["default", "admin"]
+    }
+  }
+}
+
+resource "vault_jwt_auth_backend" "github_WI" {
+  description        = "Auth from github"
+  path               = "core/github"
+  default_role       = "github_wi"
+  type               = "jwt"
+  oidc_discovery_url  = "https://token.actions.githubusercontent.com"
+  bound_issuer        = "https://token.actions.githubusercontent.com"
+
+}
+
+resource "vault_jwt_auth_backend_role" "github_WI" {
+  for_each        = local.github_access
+  backend         = vault_jwt_auth_backend.github_WI.path
+  role_name       = each.key
+  token_policies  = each.value
+  bound_claims = {
+    "repository": "Devcastops/${each.key}"
+  }
+  user_claim = "actor"
+  role_type  = "jwt"
+}
