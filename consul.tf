991e3bb7843b7667370f769d90bf9d827bda8f0a
James Sandford <sandford.james@gmail.com>
2024-11-04 20:25:33 +0000

    fix: try

--- consul.tf
+++ consul.tf
@@ -28,7 +28,7 @@ resource "vault_identity_oidc_assignment" "consul_auto_config" {
 resource "vault_identity_oidc_key" "consul_auto_config" {
   name      = "core_consul_auto_config"
   algorithm = "RS256"
-  allowed_client_ids = [vault_identity_oidc_client.consul_auto_config.client_id]
+  //allowed_client_ids = [vault_identity_oidc_client.consul_auto_config.client_id]
 }
 
 resource "vault_identity_oidc_client" "consul_auto_config" {
@@ -38,7 +38,7 @@ resource "vault_identity_oidc_client" "consul_auto_config" {
   assignments = [
     vault_identity_oidc_assignment.consul_auto_config.name
   ]
-  key = vault_identity_oidc_key.consul_auto_config.name
+  key = "core_consul_auto_config"
   id_token_ttl     = 2400
   access_token_ttl = 7200
 }
