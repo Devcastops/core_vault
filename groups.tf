9cc4308c9f4cc94b032c131e606684fd44350fde
James Sandford <sandford.james@gmail.com>
2024-12-30 16:03:20 +0000

    feat: move autoconfig removale

--- groups.tf
+++ groups.tf
@@ -5,8 +5,8 @@ resource "vault_identity_group" "core_admin" {
   member_entity_ids = [for s in keys(local.members) : vault_identity_entity.users[s].id if contains(local.members[s].groups,"core/admin")]
 }
 
-resource "vault_identity_group" "consul_auto_config" {
-  name     = "core/consul_auto_config"
-  type     = "internal"
-  policies = [vault_policy.consul_auto_config.name]
-}
+# resource "vault_identity_group" "consul_auto_config" {
+#   name     = "core/consul_auto_config"
+#   type     = "internal"
+#   policies = [vault_policy.consul_auto_config.name]
+# }
