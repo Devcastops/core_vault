257eca7314f79a39693f03e98f0ed7eca1f10c98
B-R-H <barnaby66+git@gmail.com>
2025-03-03 18:23:06 +0000

    FEAT: bootstraping Consul

--- consul.tf
+++ consul.tf
@@ -1,10 +1,9 @@
 resource "vault_consul_secret_backend" "core_consul" {
   path        = "core/consul"
   description = "Manages the Consul backend"
-
-  address = split("://",var.consul_url)[1]
-  token   = var.consul_token
-  scheme  = split("://",var.consul_url)[0]
+  address     = split("://", var.consul_url)[1]
+  scheme      = split("://", var.consul_url)[0]
+  bootstrap   = true
 }
 
 resource "vault_consul_secret_backend_role" "admin" {
