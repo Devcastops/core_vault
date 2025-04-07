401c7782ad008968163e9a238426e3f25c225613
James <james.sandford@blinkbyte.co.uk>
2025-04-07 19:39:36 +0100

    add bound_labels

--- gcp_instance_auth.tf
+++ gcp_instance_auth.tf
@@ -19,6 +19,7 @@ resource "vault_gcp_auth_backend_role" "vault_server" {
   role              = "vault_server"
   type              = "gce"
   bound_projects    = ["devcastops"]
+  bound_labels      = ["vault_server:cluster-0"]
   token_ttl         = 300
   token_max_ttl     = 600
   token_policies    = [vault_policy.ca_rotate.name]
