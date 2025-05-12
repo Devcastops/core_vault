6a91aa05677582698eb210f428dedfa9d0dde06a
James <james.sandford@blinkbyte.co.uk>
2025-05-12 19:59:42 +0100

    feat: add policie

--- gcp_instance_auth.tf
+++ gcp_instance_auth.tf
@@ -10,7 +10,7 @@ resource "vault_gcp_auth_backend_role" "client" {
   bound_projects    = ["devcastops"]
   token_ttl         = 300
   token_max_ttl     = 600
-  token_policies    = [vault_policy.consul_server.name]
+  token_policies    = [vault_policy.consul_server.name, vault_policy.consul_client_token.name]
   add_group_aliases = true
 }
 
@@ -22,6 +22,6 @@ resource "vault_gcp_auth_backend_role" "vault_server" {
   bound_labels      = ["vault_server:cluster-0"]
   token_ttl         = 300
   token_max_ttl     = 600
-  token_policies    = [vault_policy.ca_rotate.name]
+  token_policies    = [vault_policy.ca_rotate.name, vault_policy.consul_client_token.name]
   add_group_aliases = true
 }
